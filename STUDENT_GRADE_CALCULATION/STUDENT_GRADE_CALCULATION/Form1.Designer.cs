5b9ee8d63d7327ab77f3a566b811c6500a15b2a6
ilayda <91330606+ilydgrpgl@users.noreply.github.com>
2023-03-02 17:21:07 +0300

    dosya islemi ile öğrenci not sistemi
    c# ile dosya islemleriyle öğrenci kaydetme ve dosyadan kayıt çekme

---
+++ STUDENT_GRADE_CALCULATION/STUDENT_GRADE_CALCULATION/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace STUDENT_GRADE_CALCULATION
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btnhesapla = new System.Windows.Forms.Button();
+            this.Txtadsoyad = new System.Windows.Forms.TextBox();
+            this.Txtvize = new System.Windows.Forms.TextBox();
+            this.Txtfinal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Lblortalama = new System.Windows.Forms.Label();
+            this.Btngecenler = new System.Windows.Forms.Button();
+            this.Btnkalanlar = new System.Windows.Forms.Button();
+            this.Btnekle = new System.Windows.Forms.Button();
+            this.Btnhepsi = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Btnhesapla
+            // 
+            this.Btnhesapla.BackColor = System.Drawing.Color.RoyalBlue;
+            this.Btnhesapla.Location = new System.Drawing.Point(763, 97);
+            this.Btnhesapla.Margin = new System.Windows.Forms.Padding(4);
+            this.Btnhesapla.Name = "Btnhesapla";
+            this.Btnhesapla.Size = new System.Drawing.Size(133, 50);
+            this.Btnhesapla.TabIndex = 0;
+            this.Btnhesapla.Text = "Hesapla";
+            this.Btnhesapla.UseVisualStyleBackColor = false;
+            this.Btnhesapla.Click += new System.EventHandler(this.Btnhesapla_Click);
+            // 
+            // Txtadsoyad
+            // 
+            this.Txtadsoyad.Location = new System.Drawing.Point(163, 92);
+            this.Txtadsoyad.Name = "Txtadsoyad";
+            this.Txtadsoyad.Size = new System.Drawing.Size(145, 30);
+            this.Txtadsoyad.TabIndex = 1;
+            // 
+            // Txtvize
+            // 
+            this.Txtvize.Location = new System.Drawing.Point(163, 137);
+            this.Txtvize.Name = "Txtvize";
+            this.Txtvize.Size = new System.Drawing.Size(145, 30);
+            this.Txtvize.TabIndex = 2;
+            // 
+            // Txtfinal
+            // 
+            this.Txtfinal.Location = new System.Drawing.Point(163, 186);
+            this.Txtfinal.Name = "Txtfinal";
+            this.Txtfinal.Size = new System.Drawing.Size(145, 30);
+            this.Txtfinal.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ad Soyad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Vize:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Final:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ortalama";
+            // 
+            // Lblortalama
+            // 
+            this.Lblortalama.AutoSize = true;
+            this.Lblortalama.Location = new System.Drawing.Point(159, 237);
+            this.Lblortalama.Name = "Lblortalama";
+            this.Lblortalama.Size = new System.Drawing.Size(50, 23);
+            this.Lblortalama.TabIndex = 8;
+            this.Lblortalama.Text = "0000";
+            // 
+            // Btngecenler
+            // 
+            this.Btngecenler.BackColor = System.Drawing.Color.RoyalBlue;
+            this.Btngecenler.Location = new System.Drawing.Point(763, 170);
+            this.Btngecenler.Margin = new System.Windows.Forms.Padding(4);
+            this.Btngecenler.Name = "Btngecenler";
+            this.Btngecenler.Size = new System.Drawing.Size(133, 50);
+            this.Btngecenler.TabIndex = 9;
+            this.Btngecenler.Text = "Geçenler";
+            this.Btngecenler.UseVisualStyleBackColor = false;
+            this.Btngecenler.Click += new System.EventHandler(this.Btngecenler_Click);
+            // 
+            // Btnkalanlar
+            // 
+            this.Btnkalanlar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.Btnkalanlar.Location = new System.Drawing.Point(763, 240);
+            this.Btnkalanlar.Margin = new System.Windows.Forms.Padding(4);
+            this.Btnkalanlar.Name = "Btnkalanlar";
+            this.Btnkalanlar.Size = new System.Drawing.Size(133, 50);
+            this.Btnkalanlar.TabIndex = 10;
+            this.Btnkalanlar.Text = "Kalanlar";
+            this.Btnkalanlar.UseVisualStyleBackColor = false;
+            this.Btnkalanlar.Click += new System.EventHandler(this.Btnkalanlar_Click);
+            // 
+            // Btnekle
+            // 
+            this.Btnekle.BackColor = System.Drawing.Color.RoyalBlue;
+            this.Btnekle.Location = new System.Drawing.Point(763, 32);
+            this.Btnekle.Margin = new System.Windows.Forms.Padding(4);
+            this.Btnekle.Name = "Btnekle";
+            this.Btnekle.Size = new System.Drawing.Size(133, 50);
+            this.Btnekle.TabIndex = 11;
+            this.Btnekle.Text = "Ekle";
+            this.Btnekle.UseVisualStyleBackColor = false;
+            this.Btnekle.Click += new System.EventHandler(this.Btnekle_Click);
+            // 
+            // Btnhepsi
+            // 
+            this.Btnhepsi.BackColor = System.Drawing.Color.RoyalBlue;
+            this.Btnhepsi.Location = new System.Drawing.Point(763, 314);
+            this.Btnhepsi.Margin = new System.Windows.Forms.Padding(4);
+            this.Btnhepsi.Name = "Btnhepsi";
+            this.Btnhepsi.Size = new System.Drawing.Size(133, 50);
+            this.Btnhepsi.TabIndex = 12;
+            this.Btnhepsi.Text = "Tüm Liste";
+            this.Btnhepsi.UseVisualStyleBackColor = false;
+            this.Btnhepsi.Click += new System.EventHandler(this.Btnhepsi_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(370, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(331, 373);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(938, 397);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Btnhepsi);
+            this.Controls.Add(this.Btnekle);
+            this.Controls.Add(this.Btnkalanlar);
+            this.Controls.Add(this.Btngecenler);
+            this.Controls.Add(this.Lblortalama);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Txtfinal);
+            this.Controls.Add(this.Txtvize);
+            this.Controls.Add(this.Txtadsoyad);
+            this.Controls.Add(this.Btnhesapla);
+            this.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btnhesapla;
+        private System.Windows.Forms.TextBox Txtadsoyad;
+        private System.Windows.Forms.TextBox Txtvize;
+        private System.Windows.Forms.TextBox Txtfinal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Lblortalama;
+        private System.Windows.Forms.Button Btngecenler;
+        private System.Windows.Forms.Button Btnkalanlar;
+        private System.Windows.Forms.Button Btnekle;
+        private System.Windows.Forms.Button Btnhepsi;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ STUDENT_GRADE_CALCULATION/STUDENT_GRADE_CALCULATION/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace STUDENT_GRADE_CALCULATION
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int not1;
+        int not2;
+        int VİZE;
+        int FİNAL;
+        int Ortalama;
+
+        public int PuanOrtalamasi(int VİZE, int FİNAL)
+        {
+
+            not1 = Convert.ToInt16(Txtvize.Text);
+            not2 = Convert.ToInt16(Txtfinal.Text);
+            VİZE = (not1 * 40) / 100;
+            FİNAL = (not2 * 60) / 100;
+            return Ortalama = VİZE + FİNAL;
+        }
+
+        
+     
+        private void Btnekle_Click(object sender, EventArgs e)
+        {
+
+                 PuanOrtalamasi(VİZE, FİNAL);
+                StreamWriter sr = File.AppendText("C:\\Users\\Ilayda\\Desktop\\öğrenci not sistemi\\Tümöğrenciler.txt");
+                sr.Write(Txtadsoyad.Text +" ");
+                sr.Write(Txtvize.Text +" ");
+                sr.Write(Txtfinal.Text+"\n");
+                sr.Close();
+                
+            if (Ortalama >= 50)
+                  {
+                    StreamWriter sr2 = File.AppendText("C:\\Users\\Ilayda\\Desktop\\öğrenci not sistemi\\geçenler.txt");
+                    sr2.Write(Txtadsoyad.Text +" ");
+                    sr2.Write(Txtvize.Text +" ");
+                    sr2.Write(Txtfinal.Text+"\n");
+                    sr2.Close();
+                     MessageBox.Show("öğrenci bilgeleri kaydoldu. öğrenci geçmiştir");
+ 
+                   }
+            else
+            {
+                StreamWriter sr3 = File.AppendText("C:\\Users\\Ilayda\\Desktop\\öğrenci not sistemi\\kalanlar.txt" );
+                sr3.Write(Txtadsoyad.Text +" ");
+                sr3.Write(Txtvize.Text +" ");
+                sr3.Write(Txtfinal.Text+"\n");
+                sr3.Close();
+                MessageBox.Show("öğrenci bilgeleri kaydoldu.öğrenci kalmıştır.");
+            }
+  
+           
+        }
+
+        private void Btnhesapla_Click(object sender, EventArgs e)
+        {
+            PuanOrtalamasi(VİZE, FİNAL);
+            Lblortalama.Text =Ortalama.ToString();
+        }
+
+        private void Btngecenler_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            StreamReader sr = new StreamReader("C:\\Users\\Ilayda\\Desktop\\öğrenci not sistemi\\geçenler.txt");
+            while (sr.EndOfStream == false) //endofstrem=dosya sonu karakteri demektir.
+            {
+                string oku = sr.ReadLine();
+               richTextBox1.Text += oku + "\n";
+            }
+
+            sr.Close();
+        }
+
+        private void Btnkalanlar_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            StreamReader sr = new StreamReader("C:\\Users\\Ilayda\\Desktop\\öğrenci not sistemi\\kalanlar.txt");
+            while (sr.EndOfStream == false) //endofstrem=dosya sonu karakteri demektir.
+            {
+                string oku = sr.ReadLine();
+                richTextBox1.Text += oku + "\n";
+            }
+
+            sr.Close();
+        }
+
+        private void Btnhepsi_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            StreamReader sr = new StreamReader("C:\\Users\\Ilayda\\Desktop\\öğrenci not sistemi\\Tümöğrenciler.txt");
+            while (sr.EndOfStream == false) //endofstrem=dosya sonu karakteri demektir.
+            {
+                string oku = sr.ReadLine();
+                richTextBox1.Text += oku + "\n";
+            }
+
+            sr.Close();
+        }
+    }
+}
